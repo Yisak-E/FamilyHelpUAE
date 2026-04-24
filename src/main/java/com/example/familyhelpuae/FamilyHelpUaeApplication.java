70385e962136faeb13f6dde8cb32f5237be747a0
Yisak Metaferiya <yisakmetaferiya1@gmail.com>
2026-04-24 13:27:03 +0400

    FEAT: initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/com/example/familyhelpuae/FamilyHelpUaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.familyhelpuae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyHelpUaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyHelpUaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/familyhelpuae/FamilyHelpUaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familyhelpuae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyHelpUaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
